3ebe24321859ef76bc821a9922ae777120c2a456
Michel Kansou <michel.kansou@outlook.fr>
2019-04-14 12:29:26 +0200

    update use_case 2 :white_check_mark:

--- test/use_case_2.test.js
+++ test/use_case_2.test.js
@@ -2,16 +2,14 @@ import { convertToNumber, checkSum } from '../bank_ocr';
 
 describe('Testing use case 2', () => {
     test('convertToNumber : should receive path for use_case_2 with valid checksum', async () => {
-        const validChecksum = await convertToNumber('./use_case_2/valid_checksum.txt');
+        const validChecksum = await convertToNumber('./use_case_2/valid_checksum.txt', true);
 
         expect(validChecksum).toEqual('345882865');
-        expect(checkSum(validChecksum.split(''))).toEqual(true);
     });
 
     test('convertToNumber : should receive path for use_case_2 with valid checksum', async () => {
-        const invalidChecksum = await convertToNumber('./use_case_2/invalid_checksum.txt');
+        const invalidChecksum = await convertToNumber('./use_case_2/invalid_checksum.txt', true);
 
-        expect(invalidChecksum).toEqual('664371495');
-        expect(checkSum(invalidChecksum.split(''))).toEqual(false);
+        expect(invalidChecksum).toEqual('664371495 ERR');
     });
 })
